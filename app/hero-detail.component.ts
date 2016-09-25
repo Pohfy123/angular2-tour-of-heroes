86557e8d0b42d308d4987c2aba0fc1577b799655
Pohfy123 <pohfy123@gmail.com>
2016-09-26 00:15:11 +0700

    http

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,17 +1,13 @@
-// Keep the Input import for now, we'll remove it later:
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import { Hero }        from './hero';
 import { HeroService } from './hero.service';
-
-import { Hero } from './hero';
 @Component({
   selector: 'my-hero-detail',
   templateUrl: 'app/hero-detail.component.html',
-  styleUrls: ['app/hero-detail.component.css'],  
+  styleUrls: ['app/hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input()
   hero: Hero;
   constructor(
     private heroService: HeroService,
@@ -19,10 +15,14 @@ export class HeroDetailComponent implements OnInit {
   }
   ngOnInit(): void {
     this.route.params.forEach((params: Params) => {
-        let id = +params['id'];
-        this.heroService.getHero(id)
+      let id = +params['id'];
+      this.heroService.getHero(id)
         .then(hero => this.hero = hero);
     });
+  }
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(this.goBack);
   }
   goBack(): void {
     window.history.back();
